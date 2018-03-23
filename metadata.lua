85b0fe60f0c95c6c5d80796bda8770fd36d983ea
Vladimir Bro <insyon@gmail.com>
2018-03-23 22:47:51 +0100

    tag

--- metadata.lua
+++ metadata.lua
@@ -1,16 +1,16 @@
 return PlaceObj('ModDef', {
 	'title', "The Red Sandbox",
 	'description', "Adds disaster workshifts for solar panels, wind turbines, stirlings and heaters.\r\nAdds repeatable advanced researches in each field for 2000 RPs that will discover breakthrough if succed or grant small bonus per field.",
-	'tags', "research, breakthrough, disasters, sandbox",
+	'tags', "research, breakthrough, disasters, sandbox, buildings",
 	'id', "RSB",
 	'steam_id', "1334868594",
 	'author', "Vo1",
-	'version', 378,
+	'version', 381,
 	'lua_revision', 228184,
 	'code', {
 		"Code/RSBDisasterControl.lua",
 		"Code/RSBResearch.lua",
 		"Code/RSBResearchMap.lua",
 	},
-	'saved', 1521830737,
+	'saved', 1521841638,
 })
